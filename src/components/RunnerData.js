19e51820a6c69957a18d5884ce62f8e4d7e71c4d
Steven Meiklejohn <stevenmeiklejohn700@gmail.com>
2021-04-18 16:34:42 +0100

    Added list of runs to individual progress page

--- src/components/RunnerData.js
+++ src/components/RunnerData.js
@@ -8,6 +8,8 @@ const RunnerData = ({runs, selectedName}) => {
         )
     }
 
+
+
     console.log("incoming runs", runs)
     let filteredRuns = []
     for(let run of runs){
@@ -23,6 +25,14 @@ const RunnerData = ({runs, selectedName}) => {
         totalDistance += run["distance"];
     }
 
+    const runElements = filteredRuns.map((run) => {
+        let date = new Date(run.run_date);
+        let dateString = date.getDate() + "/" + (date.getMonth()+1) + "/" + date.getFullYear();
+        return <p>Distance: {run.distance}    Date: {dateString}</p>
+    })
+
+
+
 
 
     return(
@@ -30,6 +40,12 @@ const RunnerData = ({runs, selectedName}) => {
         <h1>{selectedName}</h1>
         <p> Total recorded runs: {totalNumberOfRuns}</p>
         <p> Total distance: {totalDistance}</p>
+        <br></br>
+        <br></br>
+        <br></br>
+
+        <h1>Runs</h1>
+        {runElements}
         </div>
 
     )
